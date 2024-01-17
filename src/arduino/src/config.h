bc8e8aafb3f297c0bad9805ed73c4565cac5fe52
Saint138 <93931915+Saint138@users.noreply.github.com>
2024-01-17 10:06:34 +0100

    create

---
+++ src/arduino/src/config.h
@@ -0,0 +1,24 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define LED_1 1
+#define LED_2 2
+
+#define BUT_1 4
+
+#define SERVO_PIN 3
+
+#define POT_PIN A1
+
+#define LCD_SDA_PIN A4
+#define LCD_SCL_PIN A5
+
+#define WL1 10
+#define WL2 7
+#define WL3 3
+#define WL4 1
+
+#define F1 1000
+#define F2 500
+
+#endif

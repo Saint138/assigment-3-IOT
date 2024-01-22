02fcca88b56966b9bb3fd590f4084ba3d3746b5e
nardu55 <martino.narducci@studio.unibo.it>
2024-01-22 11:12:21 +0100

    fix

--- src/arduino/src/tasks/AutomaticTask.cpp
+++ src/arduino/src/tasks/AutomaticTask.cpp
@@ -13,9 +13,6 @@ void AutomaticTask::tick() {
   // Leggi il valore dal sensore sonar (da implementare nella tua applicazione)
   int sonarValue = 0;  // Sostituire con la lettura del sensore sonar
 
-  // Adatta il valore del sonar alla posizione del servo (valore tra 0 e 180)
-  int servoPosition = map(sonarValue, 0, maxSonarValue, 0, 180);
-
   // Muovi il servo alla posizione calcolata
-  servo->move(servoPosition);
+  servo->setPosition(sonarValue);
 }
--- src/arduino/src/tasks/ManualTask.cpp
+++ src/arduino/src/tasks/ManualTask.cpp
@@ -13,9 +13,6 @@ void ManualTask::tick() {
   // Leggi il valore dal potenziometro (da implementare nella tua applicazione)
   int potValue = 0;  // Sostituire con la lettura del potenziometro
 
-  // Adatta il valore del potenziometro alla posizione del servo (valore tra 0 e 180)
-  int servoPosition = map(potValue, 0, maxPotValue, 0, 180);
-
   // Muovi il servo alla posizione calcolata
-  servo->move(servoPosition);
+  servo->setPosition(potValue);
 }

71c3bd992209ea901e031269168b9057a5801f34
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2024-01-31 12:56:42 +0100

    Modified project dependencies

--- src/River_Monitoring_Service/build.gradle.kts
+++ src/River_Monitoring_Service/build.gradle.kts
@@ -7,8 +7,10 @@
  */
 
 plugins {
+    id("application")
     // Apply the application plugin to add support for building a CLI application in Java.
-    id 'java-library'
+    id("java-library")
+    
 }
 
 repositories {
@@ -23,30 +25,30 @@ dependencies {
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
     // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api 'org.apache.commons:commons-math3:3.6.1'
+    api("org.apache.commons:commons-math3:3.6.1")
 
     //mqtt dependencies
-    implementation 'io.vertx:vertx-core:4.3.1'
-    implementation 'io.vertx:vertx-web:4.3.1'
-    implementation 'io.vertx:vertx-web-client:4.3.1'
-    implementation 'io.vertx:vertx-mqtt:4.3.1'
-    implementation group: 'com.fasterxml.jackson.core', name: 'jackson-databind', version: '2.14.2'
+    implementation("io.vertx:vertx-core:4.3.1")
+    implementation("io.vertx:vertx-web:4.3.1")
+    implementation("io.vertx:vertx-web-client:4.3.1")
+    implementation("io.vertx:vertx-mqtt:4.3.1")
+    implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.14.2")
 
     // Use JUnit test framework
-    testImplementation 'junit:junit:4.13.2'
+    testImplementation("junit:junit:4.13.2")
 
 	/* for logging */
-	implementation 'org.slf4j:slf4j-api:1.7.25'
-	implementation 'org.slf4j:slf4j-jdk14:1.7.36'
+	implementation("org.slf4j:slf4j-api:1.7.25")
+	implementation("org.slf4j:slf4j-jdk14:1.7.36")
 	
 	//Gson libs
-	implementation 'com.google.code.gson:gson:2.8.2'
+	implementation("com.google.code.gson:gson:2.8.2")
 	
 	// https://mvnrepository.com/artifact/io.github.java-native/jssc
-	implementation 'io.github.java-native:jssc:2.9.4'
+	implementation("io.github.java-native:jssc:2.9.4")
 }
 
-tasks.named('test') {
+tasks.test {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }

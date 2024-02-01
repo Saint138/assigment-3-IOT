7a9ac3427cefd53e5be37f757c17f2b03926322f
nardu55 <martino.narducci@studio.unibo.it>
2024-02-01 11:47:33 +0100

    fix variables

--- src/River_Monitoring_Service/src/main/java/river_monitoring_service/serial/SerialCommunication.java
+++ src/River_Monitoring_Service/src/main/java/river_monitoring_service/serial/SerialCommunication.java
@@ -1,55 +1,14 @@
-package src.main.java.river_monitoring_service.serial;
+package /* src.main.java. */river_monitoring_service.serial;
 
 public class SerialCommunication {
 
-	private boolean day;
-	private boolean movement;
-	private boolean lightOn;
-	private int rollerBlindsAngle;
 	private boolean automatic;
-	private boolean btCommand;	
+	private int valveOpening;	
 
-	public SerialCommunication(boolean day, boolean movement, boolean lightOn,
-	        int rollerBlindsAngle, boolean automatic, boolean btCommand) {
+	public SerialCommunication(boolean automatic, int valveOpening) {
 		super();
-		this.day = day;
-		this.movement = movement;
-		this.lightOn = lightOn;
-		this.rollerBlindsAngle = rollerBlindsAngle;
 		this.automatic = automatic;
-		this.btCommand = btCommand;
-	}
-
-	public boolean isDay() {
-		return day;
-	}
-
-	public void setDay(boolean day) {
-		this.day = day;
-	}
-
-	public boolean isMovement() {
-		return movement;
-	}
-
-	public void setMovement(boolean movement) {
-		this.movement = movement;
-	}
-
-	public boolean isLightOn() {
-		return lightOn;
-	}
-
-	public void setLightOn(boolean lightOn) {
-		this.lightOn = lightOn;
-	}
-
-	public int getRollerBlindsAngle() {
-		return rollerBlindsAngle;
-	}
-
-	public void setRollerBlindsAngle(int rollerBlindsAngle) {
-		this.rollerBlindsAngle = rollerBlindsAngle;
+		this.valveOpening = valveOpening;
 	}
 
 	public boolean isAutomatic() {
@@ -60,16 +19,16 @@ public class SerialCommunication {
 		this.automatic = automatic;
 	}
 
-	public boolean isBtCommand() {
-		return btCommand;
+	public int getValveOpening() {
+		return valveOpening;
 	}
 
-	public void setBtCommand(boolean btCommand) {
-		this.btCommand = btCommand;
+	public void setValveOpening(int valveOpening) {
+		this.valveOpening = valveOpening;
 	}
 
 	@Override
 	public String toString() {
-		return "SerialCommunication [day=" + day + ", movement=" + movement + ", lightOn=" + lightOn + ", rollerBlindsAngle=" + rollerBlindsAngle + ", automatic=" + automatic + "]";
+		return "SerialCommunication [automatic =  " + automatic + ", valveOpening = " + valveOpening + "]";
 	}
 }

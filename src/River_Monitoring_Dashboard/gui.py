d96baef4aef811a06e01f1fa10fdd884e65dc87a
nardu55 <martino.narducci@studio.unibo.it>
2024-02-10 15:52:25 +0100

    gui fix

--- src/River_Monitoring_Dashboard/gui.py
+++ src/River_Monitoring_Dashboard/gui.py
@@ -3,20 +3,23 @@ from tkinter import ttk
 from tkinter import messagebox
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import requests
 import random
 import time
 
+# dal backend arrivano 4 valori:
+# "state"(String)
+# "waterLevel"(int)
+# "dashboard"(boolean)
+# "automatic"(boolean)
+
+# Simula una richiesta HTTP per ottenere dati
 def request_data(url):
-    # Simulazione di dati di test
-    data = []
-    for i in range(10):
-        data.append({
-            'tempo': f'Timestamp {i}',
-            'altezza': random.uniform(0, 100),  # Altezza casuale tra 0 e 100
-            'apertura': random.uniform(0, 100),  # Valore casuale di apertura tra 0 e 100
-            'abilitato': random.choice([True, False])  # Valore casuale di abilitazione
-        })
+    data = {
+        'tempo': time.strftime('%H:%M:%S'),
+        'altezza': random.uniform(0, 100),  # Altezza casuale tra 0 e 100
+        'livello acqua': random.uniform(0, 100),  # Valore casuale di livello acqua tra 0 e 100
+        'abilitato': random.choice([True, False])  # Valore casuale di abilitazione
+    }
     return data
 
 """ def request_data(url):
@@ -28,51 +31,38 @@ def request_data(url):
         print(f"Errore nella richiesta: {str(e)}")
         return None """
 
-def periodic_request(urlReceive, interval, figure, canvas, servomotorLabel, stateLabel, app, ax, slider):
+def periodic_request(urlReceive, interval, figure, canvas, ax, app):
     data = request_data(urlReceive)
     if data:
         update_graph(ax, canvas, data)
-        update_label(servomotorLabel, 'Apertura', data)
-        update_label(stateLabel, 'Stato', data)             #modificare con switch
-        update_slider_state(slider, data)
 
-    app.after(interval * 1000, periodic_request, urlReceive, interval, figure, canvas, servomotorLabel, stateLabel, app, ax, slider)
-
+    app.after(interval, periodic_request, urlReceive, interval, figure, canvas, ax, app)
 
 def update_graph(ax, canvas, data):
-    x = [point['tempo'] for point in data if 'tempo' in point and 'altezza' in point]
-    y = [point['altezza'] for point in data if 'tempo' in point and 'altezza' in point]
+    lines = ax.get_lines()
+    if lines:
+        x = lines[0].get_xdata().tolist()  # Ottiene le coordinate x esistenti
+        y = lines[0].get_ydata().tolist()  # Ottiene le coordinate y esistenti
+    else:
+        x = []
+        y = []
+
+    # Aggiorna solo gli ultimi 10 punti
+    if len(x) >= 10:
+        x.pop(0)  # Rimuove il punto più vecchio
+        y.pop(0)
+    x.append(data['tempo'])  # Aggiunge il nuovo punto
+    y.append(data['livello acqua'])
 
     ax.clear()
     ax.set_title('Dati dal Server')
     ax.set_xlabel('Orario')
-    ax.set_ylabel('Altezza')
+    ax.set_ylabel('Livello acqua')
     for tick in ax.get_xticklabels():
         tick.set_rotation(45)
     ax.plot(x, y, marker='o')
     canvas.draw()
 
-def update_label(label, field, data):
-    val = next((point[field] for point in data if field in point), None)
-    if val is not None:
-        label.set(f"{field}: {val}")
-
-def toggle_slider_state(slider, button):
-    current_state = slider.cget('state')
-    if current_state == 'normal':
-        slider.configure(state='disabled')
-        button.configure(text='Abilita Barra')
-    else:
-        slider.configure(state='normal')
-        button.configure(text='Disabilita Barra')
-
-def update_slider_state(slider, data):
-    abilitato = next((point['abilitato'] for point in data if 'abilitato' in point), None)
-    if abilitato is not None:
-        if abilitato:
-            slider.configure(state='normal')
-        else:
-            slider.configure(state='disabled')
 
 def main():
     app = tk.Tk()
@@ -84,26 +74,9 @@ def main():
     ax = figure.add_subplot(1, 1, 1)
     figure.subplots_adjust(bottom=0.2)
 
-    servomotorLabel = tk.StringVar()
-    servomotorLabel.set("Apertura: N/A")
-    labelServo = tk.Label(app, textvariable=servomotorLabel, font=("Helvetica", 12), fg="blue")
-    labelServo.pack(pady=10)
-
-    stateLabel = tk.StringVar()
-    stateLabel.set("Stato: N/A")
-    labelState = tk.Label(app, textvariable=stateLabel, font=("Helvetica", 12), fg="blue")
-    labelState.pack(pady=10)
-
     urlReceive = "http://192.168.150.205:8080/api/data"
-    interval = 2
-    slider = tk.Scale(app, from_=0, to=100, orient=tk.HORIZONTAL, label="Apertura Valvola")
-    slider.configure(length=300, sliderlength=30, sliderrelief=tk.RAISED)
-    slider.pack(pady=10)
-    toggle_button = tk.Button(app, text="Abilita Barra", command=lambda: toggle_slider_state(slider, toggle_button))
-    toggle_button.config(width=15, height=2, relief=tk.RIDGE)
-    toggle_button.pack(pady=10)
-
-    periodic_request(urlReceive, interval, figure, canvas, servomotorLabel, stateLabel, app, ax, slider)
+    interval = 1000  # Ogni secondo
+    periodic_request(urlReceive, interval, figure, canvas, ax, app)
 
     app.mainloop()
 
